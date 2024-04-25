3e87417d02076d82b164adc1bf60e6427edf0722
RevBits <info@revbits.com>
2024-04-25 15:42:01 +0000

    added package for ubuntuubuntu 22

--- install.sh
+++ install.sh
@@ -6,49 +6,45 @@ sudo dpkg -i build-deb/*.deb &&
 echo "** ======================================  Installing Pm2  ====================================== **" &&
 sudo npm i -g ./pm2-master &&
 
-echo "** ======================================  Preparing postgress SQL Depenency  ====================================== **" &&
 
-cd postgresql &&
-sudo chmod +x configure &&
-./configure &&
-sudo make &&
-echo "** ======================================  Installing postgress SQL Server ====================================== **" &&
+sudo service postgres restart
 
-sudo su -c  'make install' root && 
-echo "** ======================================  Adding user postgres ====================================== **" &&
-sudo su -c  'adduser postgres' root && 
-sudo su -c 'mkdir /usr/local/pgsql/data' root && 
-sudo su -c  'chown postgres /usr/local/pgsql/data' root && 
-echo "** ======================================  Finalizing ====================================== **" &&
 
-sudo su -c '/usr/local/pgsql/bin/initdb -D /usr/local/pgsql/data' postgres && 
-sudo su - postgres -c ' /usr/local/pgsql/bin/pg_ctl -D /usr/local/pgsql/data -l logfile start'   && 
-sudo su -c '/usr/local/pgsql/bin/createdb test' postgres &&
-export PATH="$PATH:/usr/local/pgsql/bin" &&
 
-sudo sed -i 's/max_connections = 100/max_connections = 800/g' /usr/local/pgsql/data/postgresql.conf &&
-sudo sed -i 's/shared_buffers = 128MB/shared_buffers = 528MB/g' /usr/local/pgsql/data/postgresql.conf &&
-sudo su - postgres -c ' /usr/local/pgsql/bin/pg_ctl -D /usr/local/pgsql/data -l logfile stop' && 
-sudo su - postgres -c ' /usr/local/pgsql/bin/pg_ctl -D /usr/local/pgsql/data -l logfile start' && 
+echo -e "${blueb} Creating PostgreSQL database credentials. ${normal}"
 
-echo "** Installing redis server **" &&
-cd ../redis && 
-sudo make &&
-sudo make install &&
-sudo mkdir /etc/redis && 
-sudo cp redis.conf /etc/redis &&
+DB_NAME=''
+DB_USER=''
+DB_PASS=''
 
-sudo cp redis.service  /etc/systemd/system/redis.service &&
+echo -n "Please specify the database name. (default:revbits): "
+read DB_NAME
 
-echo "** Adding redis user**" &&
+echo -n "Please specify the superuser name. (default:revbits): "
+read DB_USER
 
-sudo adduser --system --group --no-create-home redis ||  echo "** Redis user already exist**" && 
+echo -n "Please specify the password. (default:revbits): "
+read DB_PASS
+if [ -z "$DB_NAME" ]; then
+  DB_NAME=revbits
+fi
 
-sudo mkdir /var/lib/redis || echo "** Skipping already exist **" && 
-sudo chown redis:redis /var/lib/redis && 
+if [ -z "$DB_USER" ]; then
+  DB_USER=revbits
+fi
 
-sudo chmod 770 /var/lib/redis  && 
+if [ -z "$DB_PASS" ]; then
+  DB_PASS=revbits
+fi
 
-sudo systemctl start redis && 
+sudo su postgres <<EOF
+createdb  $DB_NAME;
+psql -c "CREATE USER $DB_USER WITH PASSWORD '$DB_PASS';"
+psql -c "grant all privileges on database $DB_NAME to $DB_USER;"
+echo -e "${lightblueb}${1} Postgres User '$DB_USER' and database '$DB_NAME' created.${normal}"
+EOF
+sudo sed -i 's/max_connections = 100/max_connections = 800/g' /etc/postgresql/14/main/postgresql.conf
+sudo sed -i 's/shared_buffers = 128MB/shared_buffers = 528MB/g' /etc/postgresql/14/main/postgresql.conf
+sudo service postgresql restart
 
 echo "** Installation Finish**" 
